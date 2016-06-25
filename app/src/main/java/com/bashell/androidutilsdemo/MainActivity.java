b3cf6e7ff575823c47c80a24e730649582179c71
bashellwang <875799016@qq.com>
2016-06-25 14:53:13 +0800

    添加快速索引view

--- app/src/main/java/com/bashell/androidutilsdemo/MainActivity.java
+++ app/src/main/java/com/bashell/androidutilsdemo/MainActivity.java
@@ -2,10 +2,14 @@ package com.bashell.androidutilsdemo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 import android.view.Window;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.bashell.androidutilsdemo.drag.DragGroupLayout;
+import com.bashell.androidutilsdemo.quickindex.QuickIndex;
 import com.bashell.androidutilsdemo.swipe.SwipeLayoutAdapter;
 import com.bashell.androidutilsdemo.utils.Utils;
 
@@ -13,6 +17,10 @@ public class MainActivity extends Activity {
 
     private DragGroupLayout mDragLayout;
     private ListView mContentListView;
+    private QuickIndex mQuickIndexView;
+
+    private Handler mHandler = new Handler();
+    private TextView mCenterTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,5 +47,32 @@ public class MainActivity extends Activity {
 
         mContentListView = (ListView) findViewById(R.id.lv_content);
         mContentListView.setAdapter(new SwipeLayoutAdapter(MainActivity.this));
+
+        mQuickIndexView = (QuickIndex) findViewById(R.id.quick_index_view);
+        mCenterTv = (TextView) findViewById(R.id.tv_center);
+        mQuickIndexView.setmLetterTouchedListener(new QuickIndex.onLetterTouchListener() {
+            @Override
+            public void onLetterTouched(String text) {
+                Utils.showToast(MainActivity.this, text);
+                showLetter(text);
+            }
+        });
     }
+
+    private void showLetter(String text) {
+
+        mCenterTv.setVisibility(View.VISIBLE);
+        mCenterTv.setText(text);
+
+        mHandler.removeCallbacksAndMessages(null);
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mCenterTv.setVisibility(View.GONE);
+            }
+        }, 2000);
+    }
+
+
 }
+
